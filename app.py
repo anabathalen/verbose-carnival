4e5e6bf283d805ed09764527303dae939fca3e45
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-05-20 17:14:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 
 from pages.data_visualisation import show_data_visualisation_page
+from pages.welcome_page import show_welcome_page
 
 st.set_page_config(
   page_title = "Native IM-MS Data Processing Tools",
