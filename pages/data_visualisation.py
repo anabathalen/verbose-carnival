ba01da2814896b88ae1146cfa34107997fd55cee
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-05-20 17:00:37 +0100

    Update data_visualisation.py

--- pages/data_visualisation.py
+++ pages/data_visualisation.py
@@ -3,4 +3,7 @@ import pandas as pd
 import os
 
 def show_data_visualisation_page():
-  st.write('TEST')
+  st.header("Data Visualisation")
+  st.write("Plotting tools will go here.")
+  st.info("Visualisation features are under development.")
+
