e3eb5f00bb8a567f985799580a07c7602210a484
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-05-21 14:06:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import os
 
-from pages.data_visualisation import show_data_visualisation_page
-from pages.welcome_page import show_welcome_page
-
 # This is the home page content
 st.title("Welcome to My Multi-Page Streamlit App")
 
