5bbedc0a674becdf6e652954e6af99a9d3051483
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-05-21 13:53:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ import os
 
 from pages.data_visualisation import show_data_visualisation_page
 from pages.welcome_page import show_welcome_page
+
+# This is the home page content
+st.title("Welcome to My Multi-Page Streamlit App")
+
+st.write("This is the main page. Use the menu on the left to navigate to other pages.")
