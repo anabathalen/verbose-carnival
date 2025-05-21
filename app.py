f1e3cfe4ab8f462aa63e32c6b239c2befcf89bf9
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-05-21 13:52:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,3 @@ import os
 
 from pages.data_visualisation import show_data_visualisation_page
 from pages.welcome_page import show_welcome_page
-
-# Initialize session state if needed
-if "page" not in st.session_state:
-  st.session_state.page = "Welcome"
-
-with st.sidebar:
-  st.header("Navigation")
-  selected_page = st.radio(
-    "Select Page", 
-    ["Welcome", "Data Visualisation"],
-    key="page_selection",
-    index = 0 if st.session_state.page == "Welcome" else 1
-  )
