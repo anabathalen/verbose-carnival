59190bf6528b890e91d172c8b8fe3ddc37fe555e
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-06-05 12:53:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -193,22 +193,29 @@ with header:
     st.markdown("""
     <div class="main-header">
         <h1>!!NAME!! CCS Logging and Processing Tools</h1>
-        <p>Advanced tools for protein CCS analysis and IM-MS data processing</p>
+        <p>Work-in-progress tools for logging peer-reviewed protein CCS values and processing IM-MS data.</p>
     </div>
     """, unsafe_allow_html=True)
 
 with body:
     st.markdown("""
     <div class="info-card">
-        <p><strong>Welcome!</strong> This site hosts tools for logging protein CCS values and processing IM-MS data. Some stuff about why we are doing this (expanding on beveridge paper, deveoping compendium of protein CCS values hoping to understand how structure links to CCS more closely.) Building on McLean/Bush - but protein data is harder, is it native?</p>
-        <p>Please use the tools in the sidebar and provide feedback below!</p>
+        <p><strong>Welcome!</strong> This site hosts tools for logging peer-reviewed protein CCS values. While it is in development, it also hosts tools for processing IM-MS data so that users can use/test them, but these will be separated in the final version. </p>
+        
+        <p>The relationship between protein structure and collision cross section has been the subject of lots of research. Targetted (Jarrold alpha-helix work) and untargetted (Beveridge Framework). This work has established trends in native ion mobility-mass spectrometry data, a selection of which are shown in the figure below (or will be when I have done it). By building this database, we hope to learn more about this relationship, particularly for intrinsically disordered proteins where structure prediction tools are less well developed. </p>
+        
+        <p>McLean and coworkers have established a similar database for small molecules measured with drift tube-mass spectrometers, and used the results to establish trends in CCS within chemical classes which can be used to filter candidate structures in untargetted workflows ([Collision Cross Section Database], https://lab.vanderbilt.edu/mclean-group/collision-cross-section-database). This database aims to establish similar trends for larger biomolecules. Databases published by Bush, Clemmer and Xu provide CCS values for standard proteins measured on drift tube-mass spectrometers used to calibrate travelling-wave IMS measurements. We hope that by broadening the criteria (including values for any protein on any IM-MS instrument), we can generate a broader overview of protein IM-MS literature, but we acknowledge the previously noted variability in published CCS values for the same 'native' protein. </p>
+        
+        <p>If you have the time, please go to 'ccs logging' and contribute protein CCS values from peer-reviewed literature. To explore the database so far, and to view the contributors leaderboard, go to 'view data'. There will be prizes for top contributors! Feedback is strongly encouraged. Please let us know if you have any suggestions/issues in the feedback section at the bottom of the page.</p>
+
+        <p>If you use any </p>
     </div>
     """, unsafe_allow_html=True)
     
     st.markdown("""
     <div class="warning-card">
         <strong>⚠️ Work in Progress</strong><br>
-        Please sanity check all results before use.
+        These tools are still in early development. If you use any processing tools, please sanity check your results. If you feel comfortable, head to my GitHub (anabathalen) and open a pull request - I will likely accept your changes.
     </div>
     """, unsafe_allow_html=True)
 
@@ -228,8 +235,10 @@ with guide:
             "calibrate",
             "get input files",
             "process output files",
+            "calibrate atds",
             "process and plot IMS",
-            "fit major peaks"
+            "aims from twimextract",
+            "data visualisation"
         ],
         "Description": [
             "Home page and feedback.",
@@ -238,8 +247,10 @@ with guide:
             "Process calibrant data and export to IMSCal reference file and/or csv.",
             "Generate IMSCal input files from ATDs and masses.",
             "Process IMSCal output files to get arrival time ⇒ CCS conversions for your proteins.",
-            "Use arrival time ⇒ CCS conversions from 'process output files' to process and plot IMS data.",
-            "Fits major peaks in your data according to inputted parameters."
+            "Zip together all your data and calibrate it.",
+            "Scale and plot your calibrated IMS data.",
+            "Smooth, normalise and plot aIMS data.",
+            "This will be a tool for data visualisation but it doesn't exist yet."
         ]
     }
     
