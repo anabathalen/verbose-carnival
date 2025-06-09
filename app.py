77548e845e0456834b0ac576962982d05674bc2c
Ana Clara Bath Alen <ana.bathalen@postgrad.manchester.ac.uk>
2025-06-09 15:40:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -230,7 +230,6 @@ with body:
     st.markdown("""
     <div class="info-card">
         <p>By building this database, we will learn more about this relationship, particularly for intrinsically disordered proteins where structure prediction tools are less well developed. We will make this database widely available. All contributors to the database will be credited. </p>
-        
         <p>McLean and coworkers have established a similar database for small molecules measured with drift tube-mass spectrometers, and used the results to establish trends in CCS within chemical classes which can be used to filter candidate structures in untargeted workflows (<a href="https://lab.vanderbilt.edu/mclean-group/collision-cross-section-database" target="_blank">Collision Cross Section Database</a>). This database aims to establish similar trends for larger biomolecules. Resources published by Bush, Clemmer and Xu provide CCS values for standard proteins measured on drift tube-mass spectrometers used to calibrate travelling-wave IMS measurements. By broadening the criteria (including values for any protein on any IM-MS instrument), we will generate a broader overview of protein IM-MS literature. We acknowledge the previously noted variability in published CCS values for the same 'native' protein. </p> 
         <p>Click the button below to get started (or navigate to 'ccs logging'). To explore the database so far, and to view the contributors leaderboard, go to 'view data'. There will be prizes for top contributors!</p>
     </div>
@@ -238,11 +237,11 @@ with body:
 
     # Navigation button outside markdown
     if st.button("Go to CCS Logging"):
-        st.switch_page("pages/ccs_logging.py")
+        st.switch_page("pages/1_ccs_logging.py")
 
     # Navigation button outside markdown
     if st.button("Go to CCS Logging"):
-        st.switch_page("pages/view_data.py")
+        st.switch_page("pages/2_view_data.py")
 
     st.markdown("""
     <div class="info-card">
